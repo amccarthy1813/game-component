e86428e35635298fc528b5aad9f83dc0c26d5168
Aidan McCarthy <48805366+amccarthy1813@users.noreply.github.com>
2019-05-03 18:34:14 -0400

    Add files via upload
    Added support for sending and updating scores in the database

---
+++ updateScore.php
@@ -0,0 +1,26 @@
+<?php
+//get the current score
+$currentScore = intval($_POST['score']);
+
+//start session
+session_start();
+
+// connect to database
+$db = mysqli_connect('localhost', 'MIBGa_DB_admin', 'PenguinPie', 'MIBGa_DB');
+
+//get the current user of the session
+$user = $_SESSION['username'];
+
+//get a user's previous score from the database
+$getQuery = "SELECT score FROM users WHERE username = $user";
+$previousScore = intval(mysqli_query($db, $getQuery));
+
+//check if the current score is greater than the previous score
+//if it is, update the score
+if($currentScore > $previousScore) {
+	$setQuery = "UPDATE users SET score = $currentScore 
+		WHERE username = $user";
+	mysqli_query($db, $setQuery)
+}
+
+?>
